a7510594760928a7c615439a10da089098c2879d
Davis Wu <Davis Wu>
2016-10-27 11:25:11 -0400

    First commit

---
+++ Warhol.cpp
@@ -0,0 +1,12 @@
+// Warhol.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "hi" << std::endl;
+	return 0;
+}
+
